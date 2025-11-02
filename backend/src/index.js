c00f4103fa214551ffec3351a1352fdf581430a6
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2025-11-01 21:12:53 -0300

    add database connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import { connectDb } from "./config/configDb";
 
 const app = express();
 app(express.json());
@@ -18,3 +19,20 @@ app.use(
 app.get("/", (req, res) => {
   res.send("Bienvenido al sistema de inscripción de electivo.");
 });
+
+// Inicia la conexión con la base de datos
+connectDb()
+  .then(() => {
+    // Carga las rutas de la aplicación
+    routerApi(app);
+
+    // Levanta el servidor
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`=> Servidor iniciado en http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error al conectar con la base de datos:", error);
+    process.exit(1);
+  });
