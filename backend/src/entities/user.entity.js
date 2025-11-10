903d2e1d36eb706eaea78d34952d203b4aa3da8c
Cristopher <andresaedo032@gmail.com>
2025-11-10 02:19:45 -0300

    mod: modificar entidad usuario

--- backend/src/entities/user.entity.js
+++ backend/src/entities/user.entity.js
@@ -1,39 +1,46 @@
 import { EntitySchema } from "typeorm";
 
 export const User = new EntitySchema({
-  name: "User",
-  tableName: "users",
-  columns: {
-    id: {
-      primary: true,
-      type: "int",
-      generated: "increment",
+    name: "User",
+    tableName: "users",
+    columns: {
+        id: {
+            primary: true,
+            type: "int",
+            generated: "increment",
+        },
+        email: {
+            type: "varchar",
+            length: 255,
+            unique: true,
+            nullable: false,
+        },
+        rut: {
+            type: "varchar",
+            length: 12,
+            unique: true,
+            nullable: false,
+        },
+        password: {
+            type: "varchar",
+            length: 255,
+            nullable: false,
+        },
+        role: {
+            type: "enum",
+            enum: ["administrador", "jefe_carrera", "docente", "alumno"],
+            default: "alumno",
+            nullable: false
+        },
+        created_at: {
+            type: "timestamp",
+            createDate: true,
+            default: () => "CURRENT_TIMESTAMP",
+        },
+        updated_at: {
+            type: "timestamp",
+            updateDate: true,
+            default: () => "CURRENT_TIMESTAMP",
+        },
     },
-    email: {
-      type: "varchar",
-      length: 255,
-      unique: true,
-      nullable: false,
-    },
-    password: {
-      type: "varchar",
-      length: 255,
-      nullable: false,
-    },
-    role: {
-      type: "varchar",
-      default: "user",
-      nullable: false
-    },
-    created_at: {
-      type: "timestamp",
-      createDate: true,
-      default: () => "CURRENT_TIMESTAMP",
-    },
-    updated_at: {
-      type: "timestamp",
-      updateDate: true,
-      default: () => "CURRENT_TIMESTAMP",
-    },
-  },
 });
