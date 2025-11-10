fbd60aedfe4662b148cf024e8c3777bc0384acb0
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2025-11-10 01:37:25 -0300

    add requests logic

---
+++ backend/src/controllers/request.controller.js
@@ -0,0 +1,77 @@
+import {
+  handleErrorClient,
+  handleErrorServer,
+  handleSuccess,
+} from "../handlers/responseHandlers.js";
+import { createRequestService, getRequestsService, getRequestByIdService, reviewRequestService } from "../services/request.service.js";
+import { createRequestBodyValidation, reviewRequestValidation } from "../validations/request.validation.js";
+
+export async function createRequest(req, res) {
+  try {
+    const { body } = req;
+    const { error } = createRequestBodyValidation.validate(body);
+
+    if (error) return handleErrorClient(res, 400, "Parámetros inválidos", error.message);
+
+    // Verificar si ya hizo una solicitud al electivo anteriormente
+    // para evitar solicitudes duplicadas
+
+    const newRequest = await createRequestService(body);
+    handleSuccess(res, 201, "Solicitud creada exitosamente", newRequest);
+  } catch (error) {
+    handleErrorServer(res, 500, "Error al crear la solicitud", error.message);
+  }
+}
+
+// En caso de ser estudiante, obtener solo las solicitudes propias
+// En caso de ser jefe de carrera, ver todas las solicitudes
+export async function getRequests(req, res) {
+  try {
+    const requests = await getRequestsService();
+    handleSuccess(res, 200, "Solicitudes obtenidas exitosamente", requests);
+  } catch (error) {
+    handleErrorServer(res, 500, "Error al obtener las solicitudes", error.message);
+  }
+}
+
+// Igual que en el caso anterior, solo solicitudes propias para estudiante
+// Jefe de carrera puede ver todas las solictudes
+export async function getRequestById(req, res) {
+  try {
+    const { id } = req.params;
+    
+    const request = await getRequestByIdService(id);
+    handleSuccess(res, 200, "Solicitud encontrada", request);
+  } catch (error) {
+    if (error.message === "Solicitud no encontrada") {
+      handleErrorClient(res, 404, error.message);
+    } else {
+      handleErrorServer(res, 500, "Error al obtener la solicitud", error.message);
+    }
+  }
+}
+
+// Solo el jefe de carrera puede revisar solicitudes
+export async function reviewRequest(req, res) {
+  try {
+    const { body } = req;
+    const { id } = req.params;
+
+    const request = await getRequestByIdService(id);
+    if (request.status !== "pendiente") {
+      return handleErrorClient(res, 409, "La solicitud ya fue revisada");
+    }
+
+    const { error } = reviewRequestValidation.validate(body);
+    if (error) return handleErrorClient(res, 400, "Parámetros inválidos", error.message);
+
+    const updatedRequest = await reviewRequestService(id, body);
+    handleSuccess(res, 200, "Solicitud revisada exitosamente", updatedRequest);
+  } catch (error) {
+    if (error.message === "Solicitud no encontrada") {
+      handleErrorClient(res, 404, error.message);
+    } else {
+      handleErrorServer(res, 500, "Error al revisar la solicitud", error.message);
+    }
+  }
+}
---
+++ backend/src/validations/request.validation.js
@@ -0,0 +1,62 @@
+import Joi from "joi";
+
+export const createRequestBodyValidation = Joi.object({
+  studentId: Joi.number().required().integer().min(0).messages({
+    "any.required": "El ID del estudiante es obligatorio.",
+    "number.base": "El ID del estudiante debe ser un número.",
+    "number.integer": "El ID debe ser un número entero.",
+    "number.min": "El ID debe ser un entero positivo.",
+  }),
+  electiveId: Joi.number().required().integer().min(0).messages({
+    "any.required": "El ID del electivo es obligatorio.",
+    "number.base": "El ID del electivo debe ser un número.",
+    "number.integer": "El ID del electivo debe ser un número entero.",
+    "number.min": "El ID del electivo debe ser un entero positivo.",
+  }),
+  description: Joi.string().trim().required().min(5).max(300).messages({
+    "any.required": "La descripción es obligatoria.",
+    "string.base": "La descripción debe ser de tipo String.",
+    "string.empty": "La descripción no puede estar vacía.",
+    "string.min": "La descripción debe contener al menos 5 caracteres.",
+    "string.max": "La descripción puede contener hasta 300 caracteres.",
+  }),
+})
+  .unknown(false)
+  .options({ abortEarly: false })
+  .messages({ "object.unknown": "No se permiten campos adicionales." });
+
+export const reviewRequestValidation = Joi.object({
+  status: Joi.string()
+    .required()
+    .lowercase()
+    .valid("aprobado", "rechazado", "pendiente")
+    .messages({
+      "any.required": "El estado es obligatorio.",
+      "any.only": 'Solo se permiten estados "aprobado", "rechazado" y "pendiente"',
+      "string.lowercase": "El estado debe estar en minúsculas",
+    }),
+  reviewerId: Joi.number().required().integer().min(0).messages({
+    "any.required": "El ID del revisor es obligatorio.",
+    "number.base": "El ID del revisor debe ser un número.",
+    "number.integer": "El ID del revisor debe ser un número entero.",
+    "number.min": "El ID del revisor debe ser un entero positivo.",
+  }),
+  reviewComment: Joi.string()
+    .trim()
+    .min(5)
+    .max(300)
+    .when("status", {
+      is: "rechazado",
+      then: Joi.required().messages({
+        "any.required": "El comentario del revisor es obligatorio al rechazar la solicitud.",
+      }),
+    })
+    .messages({
+      "string.base": "El comentario del revisor debe ser de tipo String.",
+      "string.min": "El comentario del revisor debe contener al menos 5 caracteres.",
+      "string.max": "El comentario del revisor puede contener hasta 300 caracteres.",
+    }),
+})
+  .unknown(false)
+  .options({ abortEarly: false })
+  .messages({ "object.unknown": "No se permiten campos adicionales." });
