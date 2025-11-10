e4885eff57bfb92662a67d7563dcc6673ebe5300
Cristopher <andresaedo032@gmail.com>
2025-11-10 02:12:28 -0300

    mod: modificar auth controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,52 +1,59 @@
-import { loginService, registerService } from "../services/auth.service.js"
+import { loginService, registerService } from "../services/auth.service.js";
 import { handleErrorClient, handleErrorServer, handleSuccess } from "../handlers/responseHandlers.js";
-import { userAuthBodyValidation } from "../validations/auth.validation.js";
+import {
+    userLoginBodyValidation,
+    userRegisterBodyValidation
+} from "../validations/auth.validation.js";
 
 export async function login(req, res) {
-  try {
-    const { body } = req;
-
-    const { error } = userAuthBodyValidation.validate(body);
-
-    if (error) {
-      return handleErrorClient(res, 400, "Parámetros inválidos", error.message);
+    try {
+        const { body } = req;
+        const { error } = userLoginBodyValidation.validate(body);
+        
+        if (error) {
+            return handleErrorClient(res, 400, "Parámetros inválidos", error.message);
+        }
+        
+        const data = await loginService(body);
+        handleSuccess(res, 200, "Inicio de sesión exitoso", data);
+    } catch (error) {
+        return handleErrorClient(res, 401, error.message);
     }
-    
-    const data = await loginService(body);
-    handleSuccess(res, 200, "Inicio de sesión exitoso", data);
-  } catch (error) {
-    return handleErrorClient(res, 401, error.message);
-  }
 }
 
 export async function register(req, res) {
-  try {
-    const { body } = req;
-
-    const { error } = userAuthBodyValidation.validate(body);
-
-    if (error) {
-      return handleErrorClient(res, 400, "Parámetros invalidos", error.message);
+    try {
+        const { body } = req;
+        const { error, value } = userRegisterBodyValidation.validate(body);
+        
+        if (error) {
+            return handleErrorClient(res, 400, "Parámetros inválidos", error.message);
+        }
+        
+        const newUser = await registerService(value);
+        delete newUser.password;
+        
+        handleSuccess(res, 201, "Usuario registrado exitosamente", newUser);
+    } catch (error) {
+        if (error.code === "23505") {
+            if (error.detail?.includes("email")) {
+                return handleErrorClient(res, 409, "El email ya está registrado");
+            }
+            if (error.detail?.includes("rut")) {
+                return handleErrorClient(res, 409, "El RUT ya está registrado");
+            }
+            return handleErrorClient(res, 409, "Ya existe un usuario con estos datos");
+        } else {
+            return handleErrorServer(res, 500, "Error interno del servidor", error.message);
+        }
     }
-
-    const newUser = await registerService(body);
-    delete newUser.password;
-    handleSuccess(res, 201, "Usuario registrado exitosamente", newUser);
-  } catch (error) {
-    // Código de error de PostgreSQL, para violación de condición única (unique constraint)
-    if (error.code === "23505") {
-      handleErrorClient(res, 409, "El email ya está registrado");
-    } else {
-      handleErrorServer(res, 500, "Error interno del servidor", error.message);
-    }
-  }
 }
 
 export async function logout(req, res) {
-  try {
-    res.clearCookie("jwt", { httpOnly: true });
-    handleSuccess(res, 200, "Sesión cerrada exitosamente");
-  } catch (error) {
-    handleErrorServer(res, 500, "Error interno del servidor", error.message);
-  }
+    try {
+        res.clearCookie("jwt", { httpOnly: true });
+        handleSuccess(res, 200, "Sesión cerrada exitosamente");
+    } catch (error) {
+        handleErrorServer(res, 500, "Error interno del servidor", error.message);
+    }
 }
