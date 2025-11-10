c16f4e7197c4298f99ee6050d67fa8efed8c4588
Cristopher <andresaedo032@gmail.com>
2025-11-10 02:23:57 -0300

    mod: agregar nuevas rutas

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes.js";
-import periodoRoutes from "./periodo.routes.js"; // <-- 1. IMPORTA TUS NUEVAS RUTAS
+import periodoRoutes from "./periodo.routes.js";
+import electiveRoutes from "./elective.routes.js";
 
 export function routerApi(app) {
-  const router = Router();
-  app.use("/api", router);
-
-  router.use("/auth", authRoutes);
-  router.use("/periodos", periodoRoutes); // <-- 2. REGISTRA TUS NUEVAS RUTAS
+    const router = Router();
+    app.use("/api", router);
+    
+    router.use("/auth", authRoutes);
+    router.use("/periodos", periodoRoutes);
+    router.use("/electives", electiveRoutes);
 }
 
