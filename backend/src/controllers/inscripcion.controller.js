76743fd5c9cf19042f84ee8320a7e3c67c90f264
Sebastian <sebamedina273@gmail.com>
2025-11-11 09:40:36 -0400

    Add: Primer avance sobre mi riquisito

---
+++ backend/src/controllers/inscripcion.controller.js
@@ -0,0 +1,26 @@
+import {
+    handleErrorClient,
+    handleErrorServer,
+    handleSuccess,
+} from "../handlers/responseHandlers.js";
+
+
+export async function crearInscripcion(){
+
+}
+
+export async function obtenerInscripcion(){
+
+}
+
+export async function obtenerIncripcionId(){
+
+}
+
+export async function eliminarIncripcion(){
+
+}
+
+export async function actualizarEstado(){
+
+}
