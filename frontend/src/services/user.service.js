db34b064a14f8c591166461710ba53806a031106
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2025-11-16 17:29:42 -0300

    add get user service

---
+++ frontend/src/services/user.service.js
@@ -0,0 +1,18 @@
+import axios from "./root.service.js";
+
+export async function getUsers() {
+  try {
+    const response = await axios.get("/users");
+
+    return {
+      success: true,
+      data: response.data.data
+    }
+  } catch (error) {
+    console.error("Error en el servicio de obtener usuarios:", error.response?.data);
+    return {
+      success: false,
+      message: error.response?.data?.message || "Error al conectar con el servidor",
+    };
+  }
+}
