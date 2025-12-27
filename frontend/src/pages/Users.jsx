acf3c076cee7eceed32974236e02400d2d9dc662
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2025-12-27 14:45:03 -0300

    add edit user functionality

--- frontend/src/pages/Users.jsx
+++ frontend/src/pages/Users.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Sidebar } from "@components/Sidebar";
 import { Header } from "@components/Header";
 import { Edit2, Trash2, UserPlus } from "lucide-react";
-import { getUsers } from "@services/user.service";
+import { getUsers, editUser } from "@services/user.service";
 import { register } from "@services/auth.service";
 import { showErrorAlert } from "@helpers/sweetAlert";
 import Swal from "sweetalert2";
@@ -73,6 +73,42 @@ const Users = () => {
       showErrorAlert("Error", "No se pudo agregar el usuario");
     }
   };
+
+  const handleEditUser = async (user) => {
+    try {
+      const formValues = await editUserDialog(user);
+      if (!formValues) return;
+
+      const editResponse = await editUser(user.id, formValues);
+
+      if (editResponse.success) {
+        Swal.fire({
+          toast: true,
+          title: "Usuario actualizado exitosamente",
+          icon: "success",
+          timer: 5000,
+          timerProgressBar: true,
+          position: "bottom-end",
+          showConfirmButton: false,
+        });
+
+        await fetchUsers();
+      } else {
+        // VERIFICAR ANTES, en preConfirm del dialog!
+        if (editResponse.message.includes("RUT")) {
+          showErrorAlert("Error", "El RUT ya está registrado");
+        }
+
+        if (editResponse.message.includes("email")) {
+          showErrorAlert("Error", "El email ya está registrado");
+        }
+      }
+    } catch (error) {
+      console.error("Error al editar usuario:", error);
+      showErrorAlert("Error", "No se pudo editar el usuario");
+    }
+    
+  }
 
   return (
     <div className="min-h-screen bg-gray-100">
@@ -147,7 +183,7 @@ const Users = () => {
                       <td className="min-w-16 p-4 flex flex-row justify-center items-center gap-2">
                         {user.role !== "administrador" ? (
                           <>
-                            <button className="rounded-4xl p-2 transition-all hover:bg-gray-300/50 active:bg-gray-300">
+                            <button onClick={() => handleEditUser(user)} className="rounded-4xl p-2 transition-all hover:bg-gray-300/50 active:bg-gray-300">
                               <Edit2 className="text-blue-700 h-5 w-5" />
                             </button>
                             <button className="rounded-4xl p-2 transition-all hover:bg-gray-300/50 active:bg-gray-300">
@@ -267,6 +303,127 @@ async function addUserDialog() {
       if (!rut) {
         Swal.showValidationMessage("El rut es obligatorio");
         return false;
+      }
+
+      if (!rutRegex.test(rut)) {
+        Swal.showValidationMessage("El RUT debe ser ingresado sin puntos y con guión");
+        return false;
+      }
+
+      if (!role) {
+        Swal.showValidationMessage("El rol es obligatorio");
+        return false;
+      }
+
+      if (!roles.includes(role)) {
+        Swal.showValidationMessage("Debe seleccionar un rol válido");
+        return false;
+      }
+
+      return { username, email, password, rut, role };
+    },
+  });
+
+  if (formValues) {
+    return {
+      username: formValues.username,
+      email: formValues.email,
+      password: formValues.password,
+      rut: formValues.rut,
+      role: formValues.role,
+    };
+  }
+
+  return null;
+}
+
+async function editUserDialog(user) {
+  const { value: formValues } = await Swal.fire({
+    html:
+      '<div class="text-start">' +
+      '<p class="font-bold text-md mb-1">Editar Usuario</p>' +
+      '<p class="text-sm text-gray-500">Completa la información para editar usuario.</p>' +
+      '<div class="flex flex-col gap-4 mt-4">' +
+      // Input nombre
+      '<div class="flex flex-col gap-0.5">' +
+      '<label for="username" class="text-sm font-medium">Nombre de Usuario</label>' +
+      '<input type="text" id="username" placeholder="'+user.username+'" value="'+user.username+'" class="border border-gray-300 px-2 py-1 text-sm rounded-md outline-0 transition-all hover:shadow-sm focus:border-blue-700" autcomplete/>' +
+      "</div>" +
+      // Input email
+      '<div class="flex flex-col gap-0.5">' +
+      '<label for="email" class="text-sm font-medium">Email</label>' +
+      '<input type="email" id="email" placeholder="'+user.email+'" value="'+user.email+'" class="border border-gray-300 px-2 py-1 text-sm rounded-md outline-0 transition-all hover:shadow-sm focus:border-blue-700" />' +
+      "</div>" +
+      // Input contraseña
+      '<div class="flex flex-col gap-0.5">' +
+      '<label for="password" class="text-sm font-medium">Contraseña</label>' +
+      '<input type="password" id="password" class="border border-gray-300 px-2 py-1 text-sm rounded-md outline-0 transition-all hover:shadow-sm focus:border-blue-700" />' +
+      "</div>" +
+      // Input RUT
+      '<div class="flex flex-col gap-0.5">' +
+      '<label for="rut" class="text-sm font-medium">RUT</label>' +
+      '<input type="text" id="rut" placeholder="'+user.rut+'" value="'+user.rut+'" class="border border-gray-300 px-2 py-1 text-sm rounded-md outline-0 transition-all hover:shadow-sm focus:border-blue-700" />' +
+      "</div>" +
+      // Input rol
+      '<div class="flex flex-col gap-0.5">' +
+      '<label for="role" class="text-sm font-medium">Rol</label>' +
+      '<select id="role" class="border border-gray-300 px-2 py-1 text-sm rounded-md outline-0 transition-all hover:shadow-sm focus:border-blue-700">' +
+      '<option value="alumno" ' + (user.role === "alumno" ? 'selected' : '') + '>Alumno</option>' +
+      '<option value="docente" ' + (user.role === "docente" ? 'selected' : '') + '>Docente</option>' +
+      '<option value="jefe_carrera" ' + (user.role === "jefe_carrera" ? 'selected' : '') + '>Jefe de Carrera</option>' +
+      "</select>" +
+      // Fin formulario
+      "</div>" +
+      "</div>" +
+      "</div>",
+    confirmButtonText: "Editar",
+    confirmButtonColor: "oklch(52.7% 0.154 150.069)",
+    showCancelButton: true,
+    cancelButtonText: "Cancelar",
+    showCloseButton: true,
+    preConfirm: () => {
+      let username = document.getElementById("username").value.trim();
+      let email = document.getElementById("email").value.trim();
+      const password = document.getElementById("password").value.trim();
+      let rut = document.getElementById("rut").value.trim();
+      const role = document.getElementById("role").value.trim();
+
+      const usernameRegex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{2,75}/;
+      const rutRegex = /^\d{7,8}-[\dkK]$/;
+      const roles = ["administrador", "jefe_carrera", "docente", "alumno"];
+
+      if (!username) {
+        username = user.username;
+      }
+
+      if (username.length < 2 || username.length > 75) {
+        Swal.showValidationMessage("El nombre de usuario debe tener entre 2 y 75 caracteres");
+        return false;
+      }
+
+      if (!usernameRegex.test(username)) {
+        Swal.showValidationMessage("Solo se permiten letras y espacios en el nombre de usuario");
+        return false;
+      }
+
+      if (!email) {
+        email = user.email;
+      }
+
+      if (email.length < 5 || email.length > 254) {
+        Swal.showValidationMessage("El email debe tener entre 5 a 254 caracteres");
+        return false;
+      }
+
+      // La contraseña puede quedar vacía y no se cambia
+      // Pero si tiene al menos un caracter, entonces se debe validar correctamente
+      if (password.length > 0 && password.length < 6) {
+        Swal.showValidationMessage("La contraseña debe tener al menos 6 caracteres");
+        return false;
+      }
+
+      if (!rut) {
+        rut = user.rut;
       }
 
       if (!rutRegex.test(rut)) {
--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -33,3 +33,20 @@ export async function getUserById(id) {
     }
   }
 }
+
+export async function editUser(id, data) {
+  try {
+    const response = await axios.patch(`/users/${id}`, data);
+
+    return {
+      success: true,
+      data: response.data.data,
+    }
+  } catch (error) {
+    console.error("Error en el servicio de editar usuario:", error.response?.data);
+    return {
+      success: false,
+      message: error.response?.data?.message || "Error al conectar con el servidor"
+    }
+  }
+}
